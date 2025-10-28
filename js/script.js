feb9ccd06b522366a6dc1157051f473846b6427e
ilariamotta <ilaria.motta93@gmail.com>
2025-10-28 14:29:54 +0100

    feat: struttura

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+// Consegna:
+// Scrivi un programma che stampi i numeri da 1 a 100,
+// ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
+
+// DATI PRESENTI *numeri da 1 a 100
+
+// DATI DA RACCOGLIERE
+
+// OPERAZIONI LOGICHE
+
+// iterazioni da 1 a 100
+    // SE multiplo di 3, stampa FIZZ
+    // ALTRIMENTI SE multiplo di 5, stampa BUZZ
+    // ALTRIMENTI multiplo di 3 e 5, stampa FIZZBUZZ
+    //ripeti fino a raggiungere 100
+
+
+// OUTPUT
+
